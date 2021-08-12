9456f18d79ff552cddbc1716845ef92ed05f9e0a
albert <albert@bigSur.local>
2021-08-12 21:31:28 +0200

    rename commit -> everyday

--- app/Controllers/Http/EverydayController.ts
+++ app/Controllers/Http/EverydayController.ts
@@ -4,7 +4,7 @@ import View from '@ioc:Adonis/Core/View'
 export default class CommitController {
   public async index() {
     const data = {}
-    const html = await View.render('pages/commit', data)
+    const html = await View.render('pages/everyday', data)
     return html
   }
 }
--- app/Controllers/Http/LoginController.ts
+++ app/Controllers/Http/LoginController.ts
@@ -1,13 +1,13 @@
 import View from '@ioc:Adonis/Core/View'
 
 export default class LoginController {
-  public async get() {
+  public async loginGet() {
     const data = {}
     const html = await View.render('pages/login/get', data)
     return html
   }
 
-  public async post({ auth, request, response }) {
+  public async loginPost({ auth, request, response }) {
     const email = request.input('email')
     const password = request.input('password')
 
@@ -18,4 +18,10 @@ export default class LoginController {
       return response.badRequest('Invalid credentials')
     }
   }
+
+  public async logoutGet ({ auth, response }) {
+    await auth.use('web').logout()
+    response.redirect('/')
+  }
+
 }
