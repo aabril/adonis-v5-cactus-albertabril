763f221f431d2a0c16300c2781fe1439804302b5
albert <albert@bigSur.local>
2021-08-12 09:53:29 +0200

    * welcome controller * writings as data collection

---
+++ app/Controllers/Http/WelcomeController.ts
@@ -0,0 +1,40 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import View from '@ioc:Adonis/Core/View'
+
+const writings = [
+  {
+    'datetime': '2021-06-27 09:00:00 +0000 UTC',
+    'datestr':  '2021-06-27',
+    'link':     'https://www.takuzen.me/hugo-theme-cactus/posts/code-block-test',
+    'linkname': 'Code Block Test'
+  },
+  {
+    'datetime': '2020-06-06 09:00:00 +0000 UTC',
+    'datestr':  '2020-06-06',
+    'link':     'https://www.takuzen.me/hugo-theme-cactus/posts/2020-06-06-changes-in-java-string',
+    'linkname': 'Changes to String in java (from 1.7.0_06)'
+  },
+  {
+    'datetime': '2020-05-01 09:00:00 +0000 UTC',
+    'datestr':  '2020-05-01',
+    'link':     'https://www.takuzen.me/hugo-theme-cactus/posts/2020-05-01-algorithms-graphs',
+    'linkname': 'Algorithms - Graphs'
+  },
+  {
+    'datetime': '2020-04-29 09:00:00 +0000 UTC',
+    'datestr':  '2020-04-29',
+    'link':     'https://www.takuzen.me/hugo-theme-cactus/posts/2020-04-29-subtyping',
+    'linkname': 'Programming Language - Subtyping'
+  }
+]
+
+export default class WelcomeController {
+  public async index(ctx: HttpContextContract) {
+
+    const html = await View.render('welcome', {
+      writings: writings
+    })
+    
+    return html
+  }
+}
