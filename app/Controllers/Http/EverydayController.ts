fa1cab3bb4bd15ab19626a7c1e89e496653334fa
albert <albert@bigSur.local>
2021-08-13 22:34:48 +0200

    drafting everyday works

--- app/Controllers/Http/EverydayController.ts
+++ app/Controllers/Http/EverydayController.ts
@@ -1,9 +1,26 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import View from '@ioc:Adonis/Core/View'
 
+const days = [
+  {
+    'datetime': '2021-08-11 09:00:00 +0000 UTC',
+    'datestr':  '2021-08-11',
+    'link':     '/everyday/2021-08-11',
+    'linkname': 'adonis-cactus'
+  },
+  {
+    'datetime': '2020-08-12 09:00:00 +0000 UTC',
+    'datestr':  '2020-08-12',
+    'link':     '/everyday/2021-08-12',
+    'linkname': 'adonis-cactus-albertabril'
+  }
+]
+
 export default class CommitController {
   public async index() {
-    const data = {}
+    const data = {
+      days
+    }
     const html = await View.render('pages/everyday/index', data)
     return html
   }
